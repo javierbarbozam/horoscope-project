4da89d5de2a3b9f05105486296f610480afb3561
javierbarbozam <barboza.javier497@gmail.com>
2022-07-03 02:26:16 -0600

    Javascript changes

---
+++ js/project.js
@@ -0,0 +1,58 @@
+
+/*
+let operacion = prompt ("Digite una operacion: 1 = Suma, 2 = Resta, 3 = Multiplicación y 4 = División");
+let v1 = prompt ("Digite el primer valor");
+let v2 = prompt ("Digite el segundo valor");
+let suma = parseInt(v1) + parseInt(v2);
+let resta = v1 - v2;
+let multiplicación = v1 * v2;
+let division = v1 / v2;
+
+switch(operacion){
+    case "1": 
+        alert(v1 + " + " + v2 + " = " + suma);
+        break;
+    case "2":
+        alert(v1 + " - " + v2 + " = " + resta);
+        break;
+    case "3":
+        alert(v1 + " * " + v2 + " = " + multiplicación);
+        break;
+    case "4":
+        alert(v1 + " / " + v2 + " = " + division);
+        break;
+    default:
+        alert("Por favor digite una operación válida.");
+}
+*/
+
+let day_one = prompt("Ingrese el día de su cumpleaños");
+let month_one = prompt("Ingrese el mes de su cumpleaños");
+//let day_two = prompt("Ingrese el día de cumpleaños de su pareja");
+//let month_two = prompt("Ingrese el mes de cumpleaños de su pareja");
+
+function couple_one(day_one, month_one) {
+  return birthday_one;
+}
+/*
+Acuario = 21/01 - 19/02
+Piscis = 20/02 - 20/03
+Aries = 21/03 - 20/04
+Tauro = 21/04 - 21/05
+Géminis = 22/05 - 21/06
+Cáncer = 22/06 - 22/07
+Leo = 23/07 - 22/08
+Virgo = 23/08 - 22/09
+Libra = 23/09 - 22/10
+Escorpio = 23/10 - 22/11
+Sagitario = 23/11 - 21/12
+Capricornio = 22/12 - 20/01
+*/
+
+if (month_one == "enero"){
+  
+}
+
+function couple_two(day_two, month_two) {
+  return birthday_two;
+}
