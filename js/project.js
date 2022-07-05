6e70ae225c1d765cbae499dc7334f9959cfb1cb3
javierbarbozam <barboza.javier497@gmail.com>
2022-07-05 15:53:02 -0600

    Zodiac Signs for couples new

--- js/project.js
+++ js/project.js
@@ -44,225 +44,225 @@ Capricornio = 22/12 - 20/01
 let day_one = prompt("Ingrese el día de su cumpleaños. Ejemplo: 20");
 let month_one = prompt("Ingrese el mes de su cumpleaños. Ejemplo: enero");
 let day_two = prompt("Ingrese el día de cumpleaños de su pareja. Ejemplo: 20");
-let month_two = prompt("Ingrese el mes de cumpleaños de su pareja. Ejemplo: 20");
+let month_two = prompt("Ingrese el mes de cumpleaños de su pareja. Ejemplo: enero");
 
 function couple_one(day_one, month_one) {
-  return birthday_one;
+  return zodiac_one;
 }
 if (month_one == "enero"){ //ENERO
   if (day_one > 20){
-    birthday_one = "Acuario"
-  } else if (birthday_one = 20){
-    birthday_one = "Capricornio y Acuario"
-  } else (birthday_one = "Acuario")
+    zodiac_one = "Acuario"
+  } else if (zodiac_one = 20){
+    zodiac_one = "Capricornio y Acuario"
+  } else (zodiac_one = "Acuario")
 }
 else if (month_one == "febrero") { //FEBRERO
   if (day_one < 19){
-    birthday_one = "Acuario"
+    zodiac_one = "Acuario"
   } else if (day_one >= 20){
-    birthday_one = "Piscis"
-  } else if (birthday_one = 19)
-    birthday_one = "Acuario y Piscis"
+    zodiac_one = "Piscis"
+  } else if (zodiac_one = 19)
+    zodiac_one = "Acuario y Piscis"
 }
 else if (month_one == "marzo"){ //MARZO
   if (day_one < 20) {
-    birthday_one = "Piscis"
+    zodiac_one = "Piscis"
   } else if (day_one >= 21){
-    birthday_one = "Aries"
+    zodiac_one = "Aries"
   } else if (day_one = 20){
-    birthday_one = "Piscis y Aries"
+    zodiac_one = "Piscis y Aries"
   }
 }
 else if (month_one == "abril") { //ABRIL
   if (day_one < 21){
-    birthday_one = "Aries"
+    zodiac_one = "Aries"
   } else if(day_one >=22){
-    birthday_one = "Tauro"
+    zodiac_one = "Tauro"
   } else if (day_one = 21){
-    birthday_one = "Aries y Tauro"
+    zodiac_one = "Aries y Tauro"
   }
 }
 else if (month_one == "mayo"){ //MAYO
   if (day_one < 21){
-    birthday_one = "Tauro"
+    zodiac_one = "Tauro"
   } else if (day_one >= 22){
-    birthday_one = "Géminis"
+    zodiac_one = "Géminis"
   } else if (day_one = 21){
-    birthday_one = "Tauro y Géminis"
+    zodiac_one = "Tauro y Géminis"
   }
 }
 else if (month_one == "junio"){ //JUNIO
   if (day_one < 21){
-    birthday_one = "Géminis"
+    zodiac_one = "Géminis"
   } else if (day_one >= 22){
-    birthday_one = "Cáncer"
+    zodiac_one = "Cáncer"
   } else if (day_one = 21){
-    birthday_one = "Géminis y Cáncer"
+    zodiac_one = "Géminis y Cáncer"
   }
 }
 else if (month_one == "julio"){ //JULIO
   if (day_one < 22){
-    birthday_one = "Cáncer"
+    zodiac_one = "Cáncer"
   } else if (day_one >= 23){
-    birthday_one = "Leo"
+    zodiac_one = "Leo"
   } else if (day_one = 22){
-    birthday_one = "Cáncer y Leo"
+    zodiac_one = "Cáncer y Leo"
   }
 }
 else if (month_one == "agosto"){ //AGOSTO
   if (day_one < 22){
-    birthday_one = "Leo"
+    zodiac_one = "Leo"
   } else if (day_one >= 23){
-    birthday_one = "Virgo"
+    zodiac_one = "Virgo"
   } else if (day_one = 22){
-    birthday_one = "Leo y Virgo"
+    zodiac_one = "Leo y Virgo"
   }
 }
 else if (month_one == "septiembre"){ //SEPTIEMBRE
   if (day_one < 22){
-    birthday_one = "Virgo"
+    zodiac_one = "Virgo"
   } else if (day_one >= 23){
-    birthday_one = "Libra"
+    zodiac_one = "Libra"
   } else if (day_one = 22){
-    birthday_one = "Virgo y Libra"
+    zodiac_one = "Virgo y Libra"
   }
 }
 else if (month_one == "octubre"){ //OCTUBRE
   if (day_one < 22){
-    birthday_one = "Libra"
+    zodiac_one = "Libra"
   } else if (day_one >= 23){
-    birthday_one = "Escorpio"
+    zodiac_one = "Escorpio"
   } else if (day_one = 22){
-    birthday_one = "Libra y Escorpio"
+    zodiac_one = "Libra y Escorpio"
   }
 }
 else if (month_one == "noviembre"){ //NOVIEMBRE
   if (day_one < 22){
-    birthday_one = "Escorpio"
+    zodiac_one = "Escorpio"
   } else if (day_one >= 23){
-    birthday_one = "Sagitario"
+    zodiac_one = "Sagitario"
   } else if (day_one = 22){
-    birthday_one = "Escorpio y Sagitario"
+    zodiac_one = "Escorpio y Sagitario"
   }
 }
 else if (month_one == "diciembre"){ //DICIEMBRE
   if (day_one < 21){
-    birthday_one = "Sagitario"
+    zodiac_one = "Sagitario"
   } else if (day_one >= 22){
-    birthday_one = "Capricornio"
+    zodiac_one = "Capricornio"
   } else if (day_one = 21){
-    birthday_one = "Sagitario y Capricornio"
+    zodiac_one = "Sagitario y Capricornio"
   }
 }
 
 function couple_two(day_two, month_two) {
-  return birthday_two;
+  return zodiac_two;
 }
 if (month_two == "enero"){ //ENERO
   if (day_two > 20){
-    birthday_two = "Acuario"
-  } else if (birthday_two = 20){
-    birthday_two = "Capricornio y Acuario"
-  } else (birthday_two = "Acuario")
+    zodiac_two = "Acuario"
+  } else if (zodiac_two = 20){
+    zodiac_two = "Capricornio y Acuario"
+  } else (zodiac_two = "Acuario")
 }
 else if (month_two == "febrero") { //FEBRERO
   if (day_two < 19){
-    birthday_two = "Acuario"
+    zodiac_two = "Acuario"
   } else if (day_two >= 20){
-    birthday_two = "Piscis"
-  } else if (birthday_two = 19)
-    birthday_two = "Acuario y Piscis"
+    zodiac_two = "Piscis"
+  } else if (zodiac_two = 19)
+    zodiac_two = "Acuario y Piscis"
 }
 else if (month_two == "marzo"){ //MARZO
   if (day_two < 20) {
-    birthday_two = "Piscis"
+    zodiac_two = "Piscis"
   } else if (day_two >= 21){
-    birthday_two = "Aries"
+    zodiac_two = "Aries"
   } else if (day_two = 20){
-    birthday_two = "Piscis y Aries"
+    zodiac_two = "Piscis y Aries"
   }
 }
 else if (month_two == "abril") { //ABRIL
   if (day_two < 21){
-    birthday_two = "Aries"
+    zodiac_two = "Aries"
   } else if(day_two >=22){
-    birthday_two = "Tauro"
+    zodiac_two = "Tauro"
   } else if (day_two = 21){
-    birthday_two = "Aries y Tauro"
+    zodiac_two = "Aries y Tauro"
   }
 }
 else if (month_two == "mayo"){ //MAYO
   if (day_two < 21){
-    birthday_two = "Tauro"
+    zodiac_two = "Tauro"
   } else if (day_two >= 22){
-    birthday_two = "Géminis"
+    zodiac_two = "Géminis"
   } else if (day_two = 21){
-    birthday_two = "Tauro y Géminis"
+    zodiac_two = "Tauro y Géminis"
   }
 }
 else if (month_two == "junio"){ //JUNIO
   if (day_two < 21){
-    birthday_two = "Géminis"
+    zodiac_two = "Géminis"
   } else if (day_two >= 22){
-    birthday_two = "Cáncer"
+    zodiac_two = "Cáncer"
   } else if (day_two = 21){
-    birthday_two = "Géminis y Cáncer"
+    zodiac_two = "Géminis y Cáncer"
   }
 }
 else if (month_two == "julio"){ //JULIO
   if (day_two < 22){
-    birthday_two = "Cáncer"
+    zodiac_two = "Cáncer"
   } else if (day_two >= 23){
-    birthday_two = "Leo"
+    zodiac_two = "Leo"
   } else if (day_two = 22){
-    birthday_two = "Cáncer y Leo"
+    zodiac_two = "Cáncer y Leo"
   }
 }
 else if (month_two == "agosto"){ //AGOSTO
   if (day_two < 22){
-    birthday_two = "Leo"
+    zodiac_two = "Leo"
   } else if (day_two >= 23){
-    birthday_two = "Virgo"
+    zodiac_two = "Virgo"
   } else if (day_two = 22){
-    birthday_two = "Leo y Virgo"
+    zodiac_two = "Leo y Virgo"
   }
 }
 else if (month_two == "septiembre"){ //SEPTIEMBRE
   if (day_two < 22){
-    birthday_two = "Virgo"
+    zodiac_two = "Virgo"
   } else if (day_two >= 23){
-    birthday_two = "Libra"
+    zodiac_two = "Libra"
   } else if (day_two = 22){
-    birthday_two = "Virgo y Libra"
+    zodiac_two = "Virgo y Libra"
   }
 }
 else if (month_two == "octubre"){ //OCTUBRE
   if (day_two < 22){
-    birthday_two = "Libra"
+    zodiac_two = "Libra"
   } else if (day_two >= 23){
-    birthday_two = "Escorpio"
+    zodiac_two = "Escorpio"
   } else if (day_two = 22){
-    birthday_two = "Libra y Escorpio"
+    zodiac_two = "Libra y Escorpio"
   }
 }
 else if (month_two == "noviembre"){ //NOVIEMBRE
   if (day_two < 22){
-    birthday_two = "Escorpio"
+    zodiac_two = "Escorpio"
   } else if (day_two >= 23){
-    birthday_two = "Sagitario"
+    zodiac_two = "Sagitario"
   } else if (day_two = 22){
-    birthday_two = "Escorpio y Sagitario"
+    zodiac_two = "Escorpio y Sagitario"
   }
 }
 else if (month_two == "diciembre"){ //DICIEMBRE
   if (day_two < 21){
-    birthday_two = "Sagitario"
+    zodiac_two = "Sagitario"
   } else if (day_two >= 22){
-    birthday_two = "Capricornio"
+    zodiac_two = "Capricornio"
   } else if (day_two = 21){
-    birthday_two = "Sagitario y Capricornio"
+    zodiac_two = "Sagitario y Capricornio"
   }
 }
 
 
-document.write("Tu signo es " + birthday_one + " y el signo de tu pareja es " + birthday_two)
+document.write("Tu signo es " + zodiac_one + " y el signo de tu pareja es " + zodiac_two)
