2526b086220acd84a92436f4826aec2433d11182
javierbarbozam <barboza.javier497@gmail.com>
2022-07-05 21:51:28 -0600

    Switch deleted from code

--- js/project.js
+++ js/project.js
@@ -1,31 +1,21 @@
-/*
-Acuario = 21/01 - 19/02
-Piscis = 20/02 - 20/03
-Aries = 21/03 - 20/04
-Tauro = 21/04 - 21/05
-Géminis = 22/05 - 21/06
-Cáncer = 22/06 - 22/07
-Leo = 23/07 - 22/08
-Virgo = 23/08 - 22/09 
-Libra = 23/09 - 22/10
-Escorpio = 23/10 - 22/11 ///
-Sagitario = 23/11 - 21/12
-Capricornio = 22/12 - 20/01
-*/
-
 let day_one = prompt("Ingrese el día de su cumpleaños. Ejemplo: 20");
 let month_one = prompt("Ingrese el mes de su cumpleaños. Ejemplo: enero");
 let day_two = prompt("Ingrese el día de cumpleaños de su pareja. Ejemplo: 20");
 let month_two = prompt("Ingrese el mes de cumpleaños de su pareja. Ejemplo: enero");
-function couple_one(day_one, month_one) { //COUPLE SIGN 1
-  return zodiac_one;
-}
+let zodiac_one = ""
+let zodiac_two = ""
+let compatibility = ""
+
+//COUPLE 1
+
 if (month_one == "enero"){ //ENERO
-  if (day_one > 20){
+  if (day_one < 21){
+    zodiac_one = "Capricornio"
+  } else if (zodiac_one >= 22){
     zodiac_one = "Acuario"
-  } else if (zodiac_one = 20){
+  } else if (zodiac_one = 21){
     zodiac_one = "Capricornio y Acuario"
-  } else (zodiac_one = "Acuario")
+  }
 }
 else if (month_one == "febrero") { //FEBRERO
   if (day_one < 19){
@@ -126,9 +116,8 @@ else if (month_one == "diciembre"){ //DICIEMBRE
   }
 }
 
-function couple_two(day_two, month_two) { // COUPLE SIGN 2
-  return zodiac_two;
-}
+//COUPLE 2
+
 if (month_two == "enero"){ //ENERO
   if (day_two > 20){
     zodiac_two = "Acuario"
@@ -234,36 +223,27 @@ else if (month_two == "diciembre"){ //DICIEMBRE
     zodiac_two = "Sagitario y Capricornio"
   }
 }
+if (zodiac_one == zodiac_two){
+  compatibility = "compatibles"
+}
+
 document.write("Tu signo es " + zodiac_one + " y el signo de tu pareja es " + zodiac_two)
 
 
-function compatibility(zodiac_one, zodiac_two){ //COMPATIBILITY
-  return zodiac_match
-}
+
+
 
 /*
-let operacion = prompt ("Digite una operacion: 1 = Suma, 2 = Resta, 3 = Multiplicación y 4 = División");
-let v1 = prompt ("Digite el primer valor");
-let v2 = prompt ("Digite el segundo valor");
-let suma = parseInt(v1) + parseInt(v2);
-let resta = v1 - v2;
-let multiplicación = v1 * v2;
-let division = v1 / v2;
-
-switch(operacion){
-    case "1": 
-        alert(v1 + " + " + v2 + " = " + suma);
-        break;
-    case "2":
-        alert(v1 + " - " + v2 + " = " + resta);
-        break;
-    case "3":
-        alert(v1 + " * " + v2 + " = " + multiplicación);
-        break;
-    case "4":
-        alert(v1 + " / " + v2 + " = " + division);
-        break;
-    default:
-        alert("Por favor digite una operación válida.");
-}
+Acuario = 21/01 - 19/02
+Piscis = 20/02 - 20/03
+Aries = 21/03 - 20/04
+Tauro = 21/04 - 21/05
+Géminis = 22/05 - 21/06
+Cáncer = 22/06 - 22/07
+Leo = 23/07 - 22/08
+Virgo = 23/08 - 22/09 
+Libra = 23/09 - 22/10
+Escorpio = 23/10 - 22/11 ///
+Sagitario = 23/11 - 21/12
+Capricornio = 22/12 - 20/01
 */
